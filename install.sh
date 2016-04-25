5ee0a1da6d64189e347f7f0eb66e55fa8e9747fc
Rolf Brudeseth <rolfb@us.ibm.com>
2016-04-25 17:29:57 -0500

    Add more playbooks

--- install.sh
+++ install.sh
@@ -1,8 +1,4 @@
-dpkg -l python-pip &> /dev/null
-if (($?))
-then
-    sudo apt-get -y install python-pip
-fi
+sudo apt-get -y install python-pip python-dev
 sudo -H pip install --upgrade pip
 sudo -H pip install --upgrade setuptools
 sudo -H pip install --upgrade wheel
