4b84ca77e10b335362f7c46365fa9c489f2ffc1a
Rolf Brudeseth <rolfb@us.ibm.com>
2016-05-05 13:11:32 -0500

    Add user/password and ssh key for container

--- scripts/inventory.py
+++ scripts/inventory.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 import yaml
 import json
-f = open('inventory.yml')
+import sys
+
+f = open(sys.argv[1])
 inventory = yaml.safe_load(f)
 f.close()
-print(json.dumps(../playbooks/inventory/inventory.yml, indent=4))
+print(inventory)
