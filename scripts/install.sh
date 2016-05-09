d28dc2e47312653a81aeabc03a3e1eb834ff95a6
Jay Carman <jwcarman@us.ibm.com>
2016-05-09 11:08:20 -0500

    Fix: Additional 14.04 depencies fixed.

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,10 +1,14 @@
+DISTRIB_RELEASE=$(lsb_release -sr)
 sudo apt-get -y install python-pip python-dev
-if [[ $(lsb_release -sr) == "14.04" ]]; then
-    sudo apt-get -y install libffi-dev libssl-dev
+if [[ $DISTRIB_RELEASE == "14.04" ]]; then
+    sudo apt-get -y install libffi-dev libssl-dev lxc-dev
 fi
 sudo -H pip install --upgrade pip
 sudo -H pip install --upgrade setuptools
 sudo -H pip install --upgrade wheel
+if [[ $DISTRIB_RELEASE == "14.04" ]]; then
+    sudo -H pip install lxc-python2 
+fi
 sudo -H pip install virtualenv
 virtualenv --no-wheel --system-site-packages deployenv
 source deployenv/bin/activate
