9efc7cf33870db7ff9b62f46cb506a5047525aa6
Jay Carman <jwcarman@us.ibm.com>
2016-06-10 17:57:03 -0500

    Cobbler add distrubutions and systems automation

--- scripts/python/cobbler_add_distro.py
+++ scripts/python/cobbler_add_distro.py
@@ -8,11 +8,12 @@ import xmlrpclib
 
 from lib.logger import Logger
 
-class Ipmi(object):
+
+class CobblerAddDistro(object):
     def __init__(self, argv):
         self.log = Logger(__file__)
 
-        ARGV_MAX = 6
+        ARGV_MAX = 5
         argv_count = len(sys.argv)
         if argv_count > ARGV_MAX:
             try:
@@ -20,42 +21,95 @@ class Ipmi(object):
             except:
                 self.log.error('Invalid argument count')
                 exit(1)
-        
-        INV_FILE = sys.argv[1]
-        PATH     = sys.argv[2]
-        NAME     = sys.argv[3]
-        ARCH     = sys.argv[4]
-        
-        if ARCH == "amd64":
-            ARCH = "x86_64"
-        
+
+        PATH = sys.argv[1]
+        NAME = sys.argv[2]
+        ARCH = sys.argv[3]
+
         if len(sys.argv) == ARGV_MAX:
-            LOG_LEVEL = sys.argv[5]
+            LOG_LEVEL = sys.argv[4]
             self.log.set_level(LOG_LEVEL)
-        
+
         COBBLER_USER = 'cobbler'
         COBBLER_PASS = 'cobbler'
-        
+
         cobbler_server = xmlrpclib.Server("http://127.0.0.1/cobbler_api")
-        token = cobbler_server.login(COBBLER_USER,COBBLER_PASS)
-        
-        options = {
-            "name"  : NAME,
-            "path"  : PATH,
-            "arch"  : ARCH
-        }
-        
-        import_output = cobbler_server.background_import(options,token)
-        self.log.info(
-            "Running Cobbler import on mounted dir: " + 
-            options["path"]
-            )
-        
+        token = cobbler_server.login(COBBLER_USER, COBBLER_PASS)
+
+        new_distro_create = cobbler_server.new_distro(token)
+        cobbler_server.modify_distro(
+            new_distro_create,
+            "name",
+            NAME,
+            token)
+        if ARCH == "ppc64el":
+            cobbler_server.modify_distro(
+                new_distro_create,
+                "kernel",
+                PATH + "/install/netboot/vmlinux",
+                token)
+            cobbler_server.modify_distro(
+                new_distro_create,
+                "initrd",
+                PATH + "/install/netboot/initrd.gz",
+                token)
+        elif ARCH == "amd64":
+            cobbler_server.modify_distro(
+                new_distro_create,
+                "kernel",
+                PATH + "/install/netboot/ubuntu-installer/amd64/linux",
+                token)
+            cobbler_server.modify_distro(
+                new_distro_create,
+                "initrd",
+                PATH + "/install/netboot/ubuntu-installer/amd64/initrd.gz",
+                token)
+        cobbler_server.modify_distro(
+            new_distro_create,
+            "arch",
+            "x86_64",
+            token)
+        cobbler_server.modify_distro(
+            new_distro_create,
+            "breed",
+            "ubuntu",
+            token)
+        cobbler_server.save_distro(new_distro_create, token)
+        new_profile_create = cobbler_server.new_profile(token)
+        cobbler_server.modify_profile(
+            new_profile_create,
+            "name",
+            NAME,
+            token)
+        cobbler_server.modify_profile(
+            new_profile_create,
+            "distro",
+            NAME,
+            token)
+        cobbler_server.modify_profile(
+            new_profile_create,
+            "enable_menu",
+            "True",
+            token)
+        if ARCH == "ppc64el":
+            cobbler_server.modify_profile(
+                new_profile_create,
+                "kernel_options",
+                "console=netcfg/dhcp_timeout=1024 netcfg/choose_interface=auto ipv6.disable=1",
+                token)
+        elif ARCH == "amd64":
+            cobbler_server.modify_profile(
+                new_profile_create,
+                "kernel_options",
+                "console=tty2 console=ttyS2,115200n8 netcfg/dhcp_timeout=1024 netcfg/choose_interface=auto ipv6.disable=1",
+                token)
+        cobbler_server.save_profile(new_profile_create, token)
         cobbler_server.sync(token)
         self.log.info("Running Cobbler sync")
 
+
 def main(argv):
-    ipmi_data = Ipmi(argv)
+    cobbler_output = CobblerAddDistro(argv)
 
 if __name__ == '__main__':
     main(sys.argv)
--- scripts/python/cobbler_add_systems.py
+++ scripts/python/cobbler_add_systems.py
@@ -11,11 +11,12 @@ from lib.logger import Logger
 
 FILE_PATH = os.path.dirname(os.path.abspath(__file__))
 
-class Ipmi(object):
+
+class CobblerAddSystems(object):
     def __init__(self, argv):
 
         self.log = Logger(__file__)
-        
+
         ARGV_MAX = 3
         argv_count = len(argv)
         if argv_count > ARGV_MAX:
@@ -24,68 +25,102 @@ class Ipmi(object):
             except:
                 self.log.log_error('Invalid argument count')
                 exit(1)
-        
+
         INV_FILE = argv[1]
         if len(argv) == ARGV_MAX:
             LOG_LEVEL = argv[2]
             self.log.set_level(LOG_LEVEL)
-        
+
         COBBLER_USER = 'cobbler'
         COBBLER_PASS = 'cobbler'
-        
-        X86_PROFILE = "ubuntu-14.04.4-server-x86_64"
-        
-        DOMAIN = 'aus.stglabs.ibm.com'
-        
-        YAML_CHASSIS_SERIAL_NUMBER = 'chassis-serial-number'
-        YAML_IPV4_IPMI             = 'ipv4-ipmi'
-        YAML_USERID_IPMI           = 'userid-ipmi'
-        YAML_PASSWORD_IPMI         = 'password-ipmi'
-        YAML_HOSTNAME              = 'hostname'
-        
+
+        YAML_HOSTNAME = 'hostname'
+        YAML_IPV4_IPMI = 'ipv4-ipmi'
+        YAML_USERID_IPMI = 'userid-ipmi'
+        YAML_PASSWORD_IPMI = 'password-ipmi'
+        YAML_COBBLER_PROFILE = 'cobbler-profile'
+        YAML_IPV4_PXE = 'ipv4-pxe'
+        YAML_MAC_PXE = 'mac-pxe'
+
+        #X86_KS = "/var/lib/cobbler/kickstarts/ubuntu_1404_x86_64_sda.cfg"
+
         inventory = yaml.load(open(sys.argv[1]), Loader=AttrDictYAMLLoader)
-        
+
         cobbler_server = xmlrpclib.Server("http://127.0.0.1/cobbler_api")
-        token = cobbler_server.login(COBBLER_USER,COBBLER_PASS)
-        
+        token = cobbler_server.login(COBBLER_USER, COBBLER_PASS)
+
         node_inv = inventory['nodes']
-        
+
         for inv_key in node_inv:
             for i in range(0, len(node_inv[inv_key])):
-                NAME           = node_inv[inv_key][i][YAML_HOSTNAME]
-                HOSTNAME       = NAME + "." + DOMAIN
-                SERIAL_NUMBER  = node_inv[inv_key][i][YAML_CHASSIS_SERIAL_NUMBER]
-                IPV4_ADDR_IPMI = node_inv[inv_key][i][YAML_IPV4_IPMI]
-                USERID_IPMI    = node_inv[inv_key][i][YAML_USERID_IPMI]
-                PASSWORD_IPMI  = node_inv[inv_key][i][YAML_PASSWORD_IPMI]
-        
+
+                HOSTNAME = node_inv[inv_key][i][YAML_HOSTNAME]
+                IPV4_IPMI = node_inv[inv_key][i][YAML_IPV4_IPMI]
+                USERID_IPMI = node_inv[inv_key][i][YAML_USERID_IPMI]
+                PASSWORD_IPMI = node_inv[inv_key][i][YAML_PASSWORD_IPMI]
+                COBBLER_PROFILE = node_inv[inv_key][i][YAML_COBBLER_PROFILE]
+                IPV4_PXE = node_inv[inv_key][i][YAML_IPV4_PXE]
+                MAC_PXE = node_inv[inv_key][i][YAML_MAC_PXE]
+
                 new_system_create = cobbler_server.new_system(token)
-        
-                cobbler_server.modify_system(new_system_create,"name",NAME,token)
-                cobbler_server.modify_system(new_system_create,"hostname",HOSTNAME,token)
-                cobbler_server.modify_system(new_system_create,'modify_interface', {
-                        "macaddress-eth0"   : "01:02:03:04:05:0" + str(i),
-                        "ipaddress-eth0"    : "192.168.3." + str(10 + i),
-                        "dnsname-eth0"      : HOSTNAME,
-                }, token)
-                cobbler_server.modify_system(new_system_create,"profile",X86_PROFILE,token)
-                cobbler_server.modify_system(new_system_create,"power_address",IPV4_ADDR_IPMI,token)
-                cobbler_server.modify_system(new_system_create,"power_user",USERID_IPMI,token)
-                cobbler_server.modify_system(new_system_create,"power_pass",PASSWORD_IPMI,token)
-                cobbler_server.modify_system(new_system_create,"power_type","ipmilan",token)
-        
+
+                cobbler_server.modify_system(
+                    new_system_create,
+                    "name",
+                    HOSTNAME,
+                    token)
+                cobbler_server.modify_system(
+                    new_system_create,
+                    "hostname",
+                    HOSTNAME,
+                    token)
+                cobbler_server.modify_system(
+                    new_system_create,
+                    "power_address",
+                    IPV4_IPMI,
+                    token)
+                cobbler_server.modify_system(
+                    new_system_create,
+                    "power_user",
+                    USERID_IPMI,
+                    token)
+                cobbler_server.modify_system(
+                    new_system_create,
+                    "power_pass",
+                    PASSWORD_IPMI,
+                    token)
+                cobbler_server.modify_system(
+                    new_system_create,
+                    "power_type",
+                    "ipmilan",
+                    token)
+                cobbler_server.modify_system(
+                    new_system_create,
+                    "profile",
+                    COBBLER_PROFILE,
+                    token)
+                cobbler_server.modify_system(
+                    new_system_create,
+                    'modify_interface',
+                    {
+                        "macaddress-eth0": MAC_PXE,
+                        "ipaddress-eth0": IPV4_PXE,
+                        "dnsname-eth0": HOSTNAME},
+                    token)
+
                 cobbler_server.save_system(new_system_create, token)
-        
+
                 self.log.info(
                     "Cobbler Add System: " +
-                    "name=" + SERIAL_NUMBER + ", " +
-                    "hostname=" + HOSTNAME
+                    "name=" + HOSTNAME + ", " +
+                    "profile=" + COBBLER_PROFILE
                     )
-        
+
         cobbler_server.sync(token)
 
+
 def main(argv):
-    ipmi_data = Ipmi(argv)
+    cobbler_output = CobblerAddSystems(argv)
 
 if __name__ == '__main__':
     main(sys.argv)
