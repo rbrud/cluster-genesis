bf3d9a4b9a80dbf936ee3208db16b2d53ddc16cb
Rolf Brudeseth <rolfb@us.ibm.com>
2016-06-11 14:44:37 -0500

    Update script to get mgmt snmp info

--- scripts/python/get_mgmt_switch_config.py
+++ scripts/python/get_mgmt_switch_config.py
@@ -3,51 +3,80 @@ import sys
 import re
 import yaml
 from orderedattrdict.yamlutils import AttrDictYAMLLoader
+from orderedattrdict import AttrDict
 from pysnmp.hlapi import *
 
+from lib.utilities import *
+
 SNMP_PORT = 161
+PUBLIC = 'public'
+BRIDGE_MIB = 'BRIDGE-MIB'
+DOT_1D_TP_FDB_PORT = 'dot1dTpFdbPort'
 
-inventory = yaml.load(open(sys.argv[1]), Loader=AttrDictYAMLLoader)
-SWITCHES_MGMT_IPV4_ADDR = inventory['switches']['mgmt']['ipv4-addr']
 
-for (
-    errorIndication,
-    errorStatus,
-    errorIndex,
-    varBinds) in nextCmd(
-        SnmpEngine(),
-        CommunityData('public'),
-        UdpTransportTarget((SWITCHES_MGMT_IPV4_ADDR, SNMP_PORT)),
-        ContextData(),
-        ObjectType(ObjectIdentity('BRIDGE-MIB', 'dot1dTpFdbPort')),
-        lexicographicMode=False):
+class GetMgmtSwitchConfig(object):
+    def __init__(self, switch_mgmt_ipv4_addr, log):
+        self.mac_port = []
+        for (
+            errorIndication,
+            errorStatus,
+            errorIndex,
+            varBinds) in nextCmd(
+                SnmpEngine(),
+                CommunityData(PUBLIC),
+                UdpTransportTarget((switch_mgmt_ipv4_addr, SNMP_PORT)),
+                ContextData(),
+                ObjectType(ObjectIdentity(BRIDGE_MIB, DOT_1D_TP_FDB_PORT)),
+                lexicographicMode=False):
 
-    if errorIndication:
-        print(errorIndication)
-        break
-    elif errorStatus:
-        print('%s at %s' % (
-            errorStatus.prettyPrint(),
-            errorIndex and varBinds[int(errorIndex)-1][0] or '?'))
-        break
-    else:
-        for varBind in varBinds:
-            m = re.search(
-                ('^BRIDGE-MIB::dot1dTpFdbPort\.(' +
-                 '([\da-fA-F]{2}:){5}[\da-fA-F]{2})' +
-                 ' = ' +
-                 '(\d+)$'),
-                str(varBind))
-            mac = m.group(1)
-            port = int(m.group(3))
-            for key, value in inventory['nodes'].iteritems():
-                for i in range(0, len(inventory['nodes'][key])):
-                    if inventory['nodes'][key][i]['port-ipmi'] == port:
-                        inventory['nodes'][key][i]['mac-ipmi'] = mac
-                        continue
+            if errorIndication:
+                log.error(errorIndication)
+                sys.exit(1)
+            elif errorStatus:
+                log.error('%s at %s' % (
+                    errorStatus.prettyPrint(),
+                    errorIndex and varBinds[int(errorIndex)-1][0] or '?'))
+                sys.exit(1)
+            else:
+                _dict = AttrDict()
+                for varBind in varBinds:
+                    m = re.search(
+                        ('^%s::%s\.(' +
+                         '(%s)' +
+                         ' = ' +
+                         '(\d+)$') % (
+                             BRIDGE_MIB, DOT_1D_TP_FDB_PORT, PATTERN_MAC),
+                        str(varBind))
+                    mac = m.group(1)
+                    port = int(m.group(3))
+                    _dict['mac'] = mac
+                    _dict['port'] = port
+                    log.info('MAC %s port %d' % (mac, port))
+                    self.mac_port.append(_dict)
 
-yaml.dump(
-    inventory,
-    open(sys.argv[1], 'w'),
-    indent=4,
-    default_flow_style=False)
+    def get_mac_port(self):
+        return self.mac_port
+
+
+def main(argv):
+    from lib.logger import Logger
+    log = Logger(__file__)
+
+    ARGV_MAX = 3
+    argv_count = len(argv)
+    if argv_count > ARGV_MAX:
+        try:
+            raise Exception()
+        except:
+            self.log.error('Invalid argument count')
+            exit(1)
+
+    switch_mgmt_ipv4_addr = argv[1]
+    if len(argv) == ARGV_MAX:
+        log_level = argv[2]
+        log.set_level(log_level)
+
+    mac_port = GetMgmtSwitchConfig(switch_mgmt_ipv4_addr, log)
+
+if __name__ == '__main__':
+    main(sys.argv)
