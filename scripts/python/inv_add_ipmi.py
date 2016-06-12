216c4453473ac92dbb6837a5c1f815976334d8ad
Rolf Brudeseth <rolfb@us.ibm.com>
2016-06-12 12:39:44 -0500

    Add leases file argument and more features

--- scripts/python/inv_add_ipmi.py
+++ scripts/python/inv_add_ipmi.py
@@ -3,10 +3,12 @@ from __future__ import nested_scopes, generators, division, absolute_import, \
     with_statement, print_function, unicode_literals
 import sys
 import os.path
+from orderedattrdict import AttrDict
 
 from lib.inventory import Inventory
 from lib.logger import Logger
 from get_mgmt_switch_config import GetMgmtSwitchConfig
+from get_dhcp_lease_info import GetDhcpLeases
 
 FILE_PATH = os.path.dirname(os.path.abspath(__file__))
 
@@ -16,11 +18,12 @@ class InventoryAddIpmi(object):
         """
         Arg1: config file
         Arg2: inventory file
-        Arg3: log level
+        Arg3: DHCP leases file
+        Arg4: log level
         """
         self.log = Logger(__file__)
 
-        ARGV_MAX = 4
+        ARGV_MAX = 5
         argv_count = len(argv)
         if argv_count > ARGV_MAX:
             try:
@@ -31,13 +34,22 @@ class InventoryAddIpmi(object):
 
         CFG_FILE = argv[1]
         INV_FILE = argv[2]
+        DHCP_LEASES_FILE = argv[3]
         if len(argv) == ARGV_MAX:
-            LOG_LEVEL = argv[3]
+            LOG_LEVEL = argv[4]
             self.log.set_level(LOG_LEVEL)
 
+        dhcp_leases = GetDhcpLeases(DHCP_LEASES_FILE, self.log)
+        dhcp_mac_ip = dhcp_leases.get_mac_ip()
+
         inv = Inventory(CFG_FILE, INV_FILE, self.log)
-        mgmt_switch_config = GetMgmtSwitchConfig('192.168.3.5', self.log)
-        print(mgmt_switch_config.get_mac_port())
+        mgmt_switch_config = GetMgmtSwitchConfig(self.log)
+        mgmt_sw_cfg = AttrDict()
+        for rack, ipv4 in inv.yield_mgmt_rack_ipv4():
+            mgmt_sw_cfg[rack] = mgmt_switch_config.get_port_mac(rack, ipv4)
+
+        inv.create_nodes(dhcp_mac_ip, mgmt_sw_cfg)
+
 
 def main(argv):
     ipmi_data = InventoryAddIpmi(argv)
