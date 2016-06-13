7de854888c717be0a051e950bd6e1bfd8f378425
Rolf Brudeseth <rolfb@us.ibm.com>
2016-06-13 03:56:00 -0500

    Add IPMI data to inventory file

--- scripts/python/inv_add_ipmi_data.py
+++ scripts/python/inv_add_ipmi_data.py
@@ -13,10 +13,9 @@ FILE_PATH = os.path.dirname(os.path.abspath(__file__))
 
 class Ipmi(object):
     def __init__(self, argv):
-        inv = Inventory()
         self.log = Logger(__file__)
 
-        ARGV_MAX = 3
+        ARGV_MAX = 4
         argv_count = len(argv)
         if argv_count > ARGV_MAX:
             try:
@@ -25,9 +24,10 @@ class Ipmi(object):
                 self.log.log_error('Invalid argument count')
                 exit(1)
 
-        INV_FILE = argv[1]
+        CFG_FILE = argv[1]
+        INV_FILE = argv[2]
         if len(argv) == ARGV_MAX:
-            LOG_LEVEL = argv[2]
+            LOG_LEVEL = argv[3]
             self.log.set_level(LOG_LEVEL)
 
         SNMP_PORT = 161
@@ -41,86 +41,104 @@ class Ipmi(object):
         self.IPMI_SYSTEM_FIRMWARE = b'System Firmware'
         self.IPMI_PRODUCT_NAME = b'Product name'
         self.IPMI_OPENPOWER_FW = b'OpenPOWER Firmware'
-        self.INV_IPV4_IPMI = inv.INV_IPV4_IPMI
         self.PPC64 = b'ppc64'
 
-        inventory = inv.load(INV_FILE, self.log)
+        self.inv_obj = Inventory(CFG_FILE, INV_FILE, self.log)
 
-        node_inv = inventory['nodes']
-        for inv_key in node_inv:
-            for i in range(0, len(node_inv[inv_key])):
-                ipmi_cmd = ipmi_command.Command(
-                    bmc=node_inv[inv_key][i][inv.INV_IPV4_IPMI],
-                    userid=node_inv[inv_key][i][inv.INV_USERID_IPMI],
-                    password=node_inv[inv_key][i][inv.INV_PASSWORD_IPMI])
-                fw = ipmi_cmd.get_inventory_of_component(
-                    self.IPMI_SYSTEM_FIRMWARE)
-                try:
-                    if self.IPMI_PRODUCT_NAME in fw.keys():
-                        if (fw[self.IPMI_PRODUCT_NAME] ==
-                                self.IPMI_OPENPOWER_FW):
-                            self.get_ipmi(
-                                self.IPMI_SYSTEM_FIRMWARE,
-                                self.IPMI_PRODUCT_NAME,
-                                fw,
-                                node_inv[inv_key][i],
-                                inv.INV_ARCHITECTURE,
-                                self.PPC64)
-                except AttributeError:
-                    pass
-                for ipmi_key, ipmi_value in ipmi_cmd.get_inventory():
-                    self.log.debug('%s: %s' % (ipmi_key, ipmi_value))
-                    if ipmi_key == IPMI_SYSTEM or ipmi_key == IPMI_NODE1:
-                        self.get_ipmi(
-                            ipmi_key,
-                            self.IPMI_CHASSIS_PART_NUMBER,
-                            ipmi_value,
-                            node_inv[inv_key][i],
-                            inv.INV_CHASSIS_PART_NUMBER)
-                        self.get_ipmi(
-                            ipmi_key,
-                            self.IPMI_CHASSIS_SERIAL_NUMBER,
-                            ipmi_value,
-                            node_inv[inv_key][i],
-                            inv.INV_CHASSIS_SERIAL_NUMBER)
-                        self.get_ipmi(
-                            ipmi_key,
-                            self.IPMI_MODEL,
-                            ipmi_value,
-                            node_inv[inv_key][i],
-                            inv.INV_MODEL)
-                        self.get_ipmi(
-                            ipmi_key,
-                            self.IPMI_SERIAL_NUMBER,
-                            ipmi_value,
-                            node_inv[inv_key][i],
-                            inv.INV_SERIAL_NUMBER)
-                        if ipmi_key == IPMI_NODE1:
-                            break
+        for inv, key, _key, index, self.node in self.inv_obj.yield_nodes():
+            ipmi_cmd = ipmi_command.Command(
+                bmc=self.node[self.inv_obj.INV_IPV4_IPMI],
+                userid=self.node[self.inv_obj.INV_USERID_IPMI],
+                password=self.node[self.inv_obj.INV_PASSWORD_IPMI])
+            fw = ipmi_cmd.get_inventory_of_component(
+                self.IPMI_SYSTEM_FIRMWARE)
+            try:
+                if self.IPMI_PRODUCT_NAME in fw.keys():
+                    if (fw[self.IPMI_PRODUCT_NAME] ==
+                            self.IPMI_OPENPOWER_FW):
+                        pass
+                        value = self.get_ipmi(
+                            self.IPMI_SYSTEM_FIRMWARE,
+                            self.IPMI_PRODUCT_NAME,
+                            fw,
+                            self.PPC64)
+                        if value is not None:
+                            self.inv_obj.add_to_node(
+                                _key,
+                                index,
+                                self.inv_obj.INV_ARCHITECTURE,
+                                value)
+            except AttributeError:
+                pass
+            for ipmi_key, ipmi_value in ipmi_cmd.get_inventory():
+                self.log.debug('%s: %s' % (ipmi_key, ipmi_value))
+                if ipmi_key == IPMI_SYSTEM or ipmi_key == IPMI_NODE1:
+                    value = self.get_ipmi(
+                        ipmi_key,
+                        self.IPMI_CHASSIS_PART_NUMBER,
+                        ipmi_value)
+                    if value is not None:
+                        self.inv_obj.add_to_node(
+                            _key,
+                            index,
+                            self.inv_obj.INV_CHASSIS_PART_NUMBER,
+                            value)
+                    value = self.get_ipmi(
+                        ipmi_key,
+                        self.IPMI_CHASSIS_SERIAL_NUMBER,
+                        ipmi_value)
+                    if value is not None:
+                        self.inv_obj.add_to_node(
+                            _key,
+                            index,
+                            self.inv_obj.INV_CHASSIS_SERIAL_NUMBER,
+                            value)
+                    value = self.get_ipmi(
+                        ipmi_key,
+                        self.IPMI_MODEL,
+                        ipmi_value)
+                    if value is not None:
+                        self.inv_obj.add_to_node(
+                            _key,
+                            index,
+                            self.inv_obj.INV_MODEL,
+                            value)
+                    value = self.get_ipmi(
+                        ipmi_key,
+                        self.IPMI_SERIAL_NUMBER,
+                        ipmi_value)
+                    if value is not None:
+                        self.inv_obj.add_to_node(
+                            _key,
+                            index,
+                            self.inv_obj.INV_SERIAL_NUMBER,
+                            value)
+                    if ipmi_key == IPMI_NODE1:
+                        break
 
-        inv.dump(inventory, self.log)
+        self.inv_obj.dump()
 
     def get_ipmi(
         self,
-        ipmi_key, ipmi_field, ipmi_value,
-            inv, inv_field, inv_value=None):
+            ipmi_key, ipmi_field, ipmi_value, inv_value=None):
         if ipmi_field in ipmi_value:
             self.log.info(
-                inv[self.INV_IPV4_IPMI] +
+                self.node[self.inv_obj.INV_IPV4_IPMI] +
                 ": '" +
                 ipmi_key + '[' + ipmi_field + ']' +
                 "' = " +
                 ipmi_value[ipmi_field])
             if inv_value:
-                inv[inv_field] = inv_value
+                return inv_value
             else:
-                inv[inv_field] = str(ipmi_value[ipmi_field])
+                return str(ipmi_value[ipmi_field])
         else:
             self.log.info(
-                inv[self.INV_IPV4_IPMI] +
+                self.node[self.inv_obj.INV_IPV4_IPMI] +
                 ": '" +
                 ipmi_key + '[' + ipmi_field + ']' +
                 "' not found")
+            return None
 
 
 def main(argv):
--- scripts/python/lib/inventory.py
+++ scripts/python/lib/inventory.py
@@ -21,6 +21,8 @@ CFG_IPMI_PORTS = 'ipmi-ports'
 CFG_PXE_PORTS = 'pxe-ports'
 CFG_ETH10_PORTS = 'eth10-ports'
 CFG_ETH11_PORTS = 'eth11-ports'
+CFG_USERID_IPMI = 'userid-ipmi'
+CFG_PASSWORD_IPMI = 'password-ipmi'
 
 INV_SWITCHES = 'switches'
 INV_MGMT = 'mgmt'
@@ -34,10 +36,14 @@ INV_HOSTNAME = 'hostname'
 INV_PORT_IPMI = 'port-ipmi'
 INV_PORT_PXE = 'port-pxe'
 INV_IPV4_IPMI = 'ipv4-ipmi'
+INV_IPV4_PXE = 'ipv4-pxe'
 INV_MAC_IPMI = 'mac-ipmi'
+INV_MAC_PXE = 'mac-pxe'
 INV_RACK_ID = 'rack-id'
 INV_PORT_ETH10 = 'port-eth10'
 INV_PORT_ETH11 = 'port-eth11'
+INV_USERID_IPMI = 'userid-ipmi'
+INV_PASSWORD_IPMI = 'password-ipmi'
 
 
 class Inventory():
@@ -54,6 +60,10 @@ class Inventory():
         self.log = log
         self.cfg_file = self._get_abs_path(cfg_file)
         self.inv_file = self._get_abs_path(inv_file)
+        try:
+            self.inv = self.load(self.inv_file)
+        except:
+            pass
 
     @staticmethod
     def _get_abs_path(file):
@@ -69,7 +79,9 @@ class Inventory():
             self.log.error('Could not load file: ' + file)
             sys.exit(1)
 
-    def dump(self, inventory):
+    def dump(self, inventory=None):
+        if inventory is None:
+            inventory = self.inv
         try:
             yaml.dump(
                 inventory,
@@ -142,7 +154,7 @@ class Inventory():
 
     def create_nodes(self, dhcp_mac_ip, mgmt_switch_config):
         self.cfg = self.load(self.cfg_file)
-        inv = self.load(self.inv_file)
+        inv = self.inv
         inv[INV_NODES] = None
         _dict = AttrDict()
         for key, value in self.cfg[CFG_NODES_TEMPLATES].items():
@@ -150,7 +162,6 @@ class Inventory():
             for rack, ipmi_ports in value[CFG_IPMI_PORTS].items():
                 _list = []
                 for port_index, ipmi_port in enumerate(ipmi_ports):
-                    print(port_index)
                     for mgmt_port in mgmt_switch_config[rack]:
                         if ipmi_port in mgmt_port.keys():
                             if mgmt_port[ipmi_port] in dhcp_mac_ip:
@@ -163,6 +174,10 @@ class Inventory():
                                         value[CFG_HOSTNAME]
                                 index += 1
                                 node_dict[INV_HOSTNAME] += '-' + str(index)
+                                node_dict[INV_USERID_IPMI] = \
+                                    self.cfg[CFG_NODES_TEMPLATES][key][CFG_USERID_IPMI]
+                                node_dict[INV_PASSWORD_IPMI] = \
+                                    self.cfg[CFG_NODES_TEMPLATES][key][CFG_PASSWORD_IPMI]
                                 node_dict[INV_PORT_IPMI] = ipmi_port
                                 node_dict[INV_PORT_PXE] = \
                                     value[CFG_PXE_PORTS][rack][port_index]
@@ -170,12 +185,42 @@ class Inventory():
                                     value[CFG_ETH10_PORTS][rack][port_index]
                                 node_dict[INV_PORT_ETH11] = \
                                     value[CFG_ETH11_PORTS][rack][port_index]
+                                node_dict[INV_MAC_IPMI] = mgmt_port[ipmi_port]
                                 node_dict[INV_IPV4_IPMI] = \
                                     dhcp_mac_ip[mgmt_port[ipmi_port]]
-                                node_dict[INV_MAC_IPMI] = mgmt_port[ipmi_port]
                                 node_dict[INV_RACK_ID] = rack
                                 _list.append(node_dict)
                                 _dict[key] = _list
                                 inv[INV_NODES] = _dict
 
         self.dump(inv)
+
+    def add_pxe(self, dhcp_mac_ip, mgmt_switch_config):
+        self.cfg = self.load(self.cfg_file)
+        inv = self.inv
+        _dict = AttrDict()
+        for key, value in self.cfg[CFG_NODES_TEMPLATES].items():
+            index = 0
+            for rack, pxe_ports in value[CFG_PXE_PORTS].items():
+                _list = []
+                for port_index, pxe_port in enumerate(pxe_ports):
+                    for mgmt_port in mgmt_switch_config[rack]:
+                        if pxe_port in mgmt_port.keys():
+                            if mgmt_port[pxe_port] in dhcp_mac_ip:
+                                 port = inv[INV_NODES][key][port_index][INV_PORT_PXE]
+                                 inv[INV_NODES][key][port_index][INV_MAC_PXE] = \
+                                     mgmt_port[pxe_port]
+                                 inv[INV_NODES][key][port_index][INV_IPV4_PXE] = \
+                                     dhcp_mac_ip[mgmt_port[pxe_port]]
+
+        self.dump(inv)
+
+    def yield_nodes(self):
+        inv = self.inv
+        for key, value in inv[INV_NODES].items():
+            for index, node in enumerate(value):
+                yield inv, INV_NODES, key, index, node
+
+    def add_to_node(self, key, index, field, value):
+        inv = self.inv
+        inv[INV_NODES][key][index][field] = value
