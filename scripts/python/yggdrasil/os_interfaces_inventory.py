77935179c3e1fdd8621237dd25ae9f5923c5059d
Samuel Matzek <smatzek@us.ibm.com>
2016-06-16 13:32:33 -0500

    Support Operating System Interface Config
    This commit delivers support to configure operating
    system interface networking.  It does so using and Anisble
    dynamic inventory program that transforms the inventory file
    into vars and host vars as well as the host group itself.
    
    An ansible playbook with a jinja template file are then used
    to write the /etc/network/interfaces file on each host.
    
    The support for renaming interfaces also exists in the playbook
    but is currently disabled due to issues with inconsistent
    interface naming.  This will be enhanced in future commits.
    
    Automated unit tests are also added for all of the Python
    code.

---
+++ scripts/python/yggdrasil/os_interfaces_inventory.py
@@ -0,0 +1,227 @@
+#!/usr/bin/python
+# Copyright 2016, IBM US, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# This is an ansible dynamic inventory module which loads an inventory file
+# describing networks, nodes, and their relationships and outputs ansible
+# inventory containing the hosts and network information required to
+# allow the configuration of the network interfaces on the nodes.
+# An example output of this generation is:
+# {
+#     "mygroup": {
+#         "hosts": [
+#             "localhost"
+#         ],
+#         "vars": {
+#             "networks": {
+#                 "stg-net": {
+#                     "description": "OS storage network description",
+#                     "bridge": "br-storage",
+#                     "method": "static",
+#                     "vlan": 30,
+#                     "eth-port": "eth10",
+#                     "netmask": "255.255.255.252",
+#                     "tcp_segmentation_offload": "off"
+#                 },
+#                 "mgmt-net": {
+#                     "description": "OS mgmt network description",
+#                     "bridge": "br-mgmt",
+#                     "method": "static",
+#                     "vlan": 10,
+#                     "eth-port": "eth10",
+#                     "netmask": "255.255.255.252",
+#                     "tcp_segmentation_offload": "off"
+#                 },
+#                 "br-vlan": {
+#                     "description": "OS vxlan network description",
+#                     "bridge": "br-vlan",
+#                     "method": "static",
+#                     "eth-port": "eth11"
+#                 },
+#                 "external1": {
+#                     "description": "OS storage network description",
+#                     "network": "10.5.1.0",
+#                     "method": "static",
+#                     "eth-port": "eth10",
+#                     "netmask": "255.255.255.0",
+#                     "dns-search": "myco.domain.com",
+#                     "dns-nameservers": "10.5.1.200",
+#                     "gateway": "10.5.1.1",
+#                     "broadcast": "10.5.1.255"
+#                 },
+#                 "external2": {
+#                     "description": "eth11 10gbit network",
+#                     "method": "static",
+#                     "eth-port": "eth11"
+#                 }
+#             },
+#             'node_templates': {
+#                 'compute': {
+#                     'rename-interfaces':
+#                         [{'eth0': 'eth20'},
+#                         {'eth2': 'eth30'}]
+#                     }
+#             }
+#         }
+#     },
+#     "_meta": {
+#         "hostvars": {
+#             "localhost": {
+#                 "host_networks": {
+#                     "mgmt-net": {
+#                         "addr": "172.244.5.10"
+#                     },
+#                     "stg-net": {
+#                         "addr": "172.270.200.10"
+#                     },
+#                     "br-vlan": {
+#                         "addr": "0.0.0.0"
+#                     },
+#                     "external1": {
+#                         "addr": "10.5.1.7"
+#                     },
+#                     "external2": {}
+#                 },
+#                 'template': 'compute'
+#             }
+#         }
+#     }
+# }
+
+
+import argparse
+import copy
+import json
+import netaddr
+import sys
+import yaml
+
+# The key in on nodes in the source inventory file that contains
+# the IP address ansible should use for the host.
+HOST_IP_KEY = 'ipv4-pxe'
+# The IP address value used when a node needs an interface on the network
+# without an IP address assigned.
+INPUT_FILE = '/var/oprc/inventory.yml'
+
+
+def load_input_file():
+    with open(INPUT_FILE, 'r') as stream:
+        try:
+            return yaml.safe_load(stream)
+        except yaml.YAMLError as ex:
+            print(ex)
+            sys.exit(1)
+
+
+def get_host_ip_to_node(inventory_source):
+    # Process the inventory file and make a map of the IP address ansible
+    # will use for the host communication to the node dictionary from
+    # the input inventory.
+    ip_to_node = {}
+
+    # Flatten the nodes from the inventory into a single list
+    nodes = [node for sublist in inventory_source['nodes'].values() for node
+             in sublist]
+    for node in nodes:
+        if HOST_IP_KEY in node:
+            ip_to_node[node[HOST_IP_KEY]] = node
+    return ip_to_node
+
+
+def populate_hosts(inventory, hosts):
+    # Add the hosts to the all group and add empty dictionaries for host vars
+    # host networks
+    inventory['all']['hosts'] = hosts
+    host_vars = inventory['_meta']['hostvars']
+    for host in hosts:
+        host_vars[host] = {'host_networks': {}}
+
+
+def populate_network_variables(inventory, inventory_source):
+    # Add the networks from the inventory source into the host_vars
+    networks = copy.deepcopy(inventory_source['networks'])
+    for network in networks.values():
+        # Take the addr out and replace it with the network and the netmask
+        addr = network.pop('addr', None)
+        if addr:
+            ip = netaddr.IPNetwork(addr)
+            if ip.prefixlen != 1:
+                # We don't put networks in with prefix length == 1 because
+                # the inventory file uses this to note that while the host
+                # has an interface connected to this network, that interface
+                # does not directly get an IP address and the address goes
+                # on a bridge.
+                network['network'] = str(ip.network)
+                network['netmask'] = str(ip.netmask)
+    inventory['all']['vars'] = {'networks': networks}
+
+
+def populate_host_networks(inventory, net_list, ip_to_node):
+    hostvars = inventory['_meta']['hostvars']
+    for ip, node in ip_to_node.iteritems():
+        for net in net_list:
+            node_ip_addr = node.get(net+'-addr')
+            # If the node is connected to this network
+            if node_ip_addr is not None:
+                net_addr = {'addr': node_ip_addr}
+                # The IP address may be the empty string if the system
+                # must have the interface but does not have an IP on the
+                # interface, and bridges have the IPs.
+                if not net_addr['addr']:
+                    net_addr = {}
+                hostvars[ip]['host_networks'][net] = net_addr
+
+
+def populate_rename_interfaces(inventory, inventory_source, ip_to_node):
+    av = inventory['all']['vars']
+    av['node_templates'] = inventory_source['node_templates']
+    for ip, node in ip_to_node.iteritems():
+        inventory['_meta']['hostvars'][ip]['template'] = node['template']
+
+
+def generate_dynamic_inventory():
+
+    inventory_source = load_input_file()
+    ip_to_node = get_host_ip_to_node(inventory_source)
+    # initialize the empty inventory
+    inventory = {'all': {'hosts': [],
+                         'vars': {}
+                         },
+                 '_meta': {'hostvars': {}}}
+    populate_hosts(inventory, ip_to_node.keys())
+    populate_network_variables(inventory, inventory_source)
+    populate_host_networks(inventory, inventory_source['networks'].keys(),
+                           ip_to_node)
+    populate_rename_interfaces(inventory, inventory_source, ip_to_node)
+    return inventory
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--list', action='store_true')
+    parser.add_argument('--host', action='store')
+    args = parser.parse_args()
+    if args.list:
+        inventory = generate_dynamic_inventory()
+    else:
+        # We don't use the host argument because our inventory
+        # returns all host variables in _meta when called with --list.
+        # For any other arguments passed, just return this empty inventory.
+        inventory = {'_meta': {'hostvars': {}}}
+
+    return json.dumps(inventory)
+
+
+if __name__ == '__main__':
+    print main()
---
+++ tests/unit/test_os_interfaces_inventory.py
@@ -0,0 +1,204 @@
+# Copyright 2016, IBM US, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import copy
+from yggdrasil import os_interfaces_inventory as test_mod
+import mock
+import unittest
+
+TEST_PKG_MOD = 'yggdrasil.os_interfaces_inventory'
+
+class TestOSInterfacesInventory(unittest.TestCase):
+
+    def test_get_host_ip_to_node(self):
+        source = {'nodes': {'type1': [{test_mod.HOST_IP_KEY: 'myIP',
+                                       'otherkey': 'otherval1'},
+                                      {test_mod.HOST_IP_KEY: 'myIP2',
+                                       'otherkey': 'otherval2'},
+                                      {test_mod.HOST_IP_KEY: 'myIP3',
+                                       'otherkey': 'otherval3'},
+                                      {'no_ip_key': 'something',
+                                       'otherkey': 'otherval4'}],
+                            'type2': [{test_mod.HOST_IP_KEY: 'myIP4',
+                                       'otherkey': 'otherval5'},
+                                      {test_mod.HOST_IP_KEY: 'myIP5',
+                                       'otherkey': 'otherval6'},
+                                      {test_mod.HOST_IP_KEY: 'myIP6',
+                                       'otherkey': 'otherval7'},
+                                      {'no_ip_key': 'something',
+                                       'otherkey': 'otherval8'}]
+                            }
+                  }
+
+        ret = test_mod.get_host_ip_to_node(source)
+        expected = {'myIP': {test_mod.HOST_IP_KEY: 'myIP',
+                             'otherkey': 'otherval1'},
+                    'myIP2': {test_mod.HOST_IP_KEY: 'myIP2',
+                              'otherkey': 'otherval2'},
+                    'myIP3': {test_mod.HOST_IP_KEY: 'myIP3',
+                              'otherkey': 'otherval3'},
+                    'myIP4': {test_mod.HOST_IP_KEY: 'myIP4',
+                              'otherkey': 'otherval5'},
+                    'myIP5': {test_mod.HOST_IP_KEY: 'myIP5',
+                              'otherkey': 'otherval6'},
+                    'myIP6': {test_mod.HOST_IP_KEY: 'myIP6',
+                              'otherkey': 'otherval7'}}
+        #self.maxDiff = None
+        self.assertDictEqual(ret, expected)
+
+    def test_populate_hosts(self):
+        inventory = {'all': {'hosts': [],
+                             'vars': {}
+                             },
+                     '_meta': {'hostvars': {}}
+                     }
+        hosts = ['a', 'b', 'c']
+        test_mod.populate_hosts(inventory, hosts)
+        expected = {'all': {'hosts': ['a', 'b', 'c'],
+                            'vars': {}
+                            },
+                    '_meta': {'hostvars': {'a': {'host_networks': {}},
+                                           'b': {'host_networks': {}},
+                                           'c': {'host_networks': {}}}}}
+        self.assertDictEqual(inventory, expected)
+
+    def test_populate_network_variables(self):
+        inventory = {'all': {'hosts': [],
+                             'vars': {}
+                             },
+                     '_meta': {'hostvars': {}}
+                     }
+        expected_output = copy.deepcopy(inventory)
+        inv_src = {'networks': {'net1': {'addr': '10.5.1.5/22',
+                                         'otherkey': 'otherval'},
+                                'net2': {'addr': '0.0.0.0/1',
+                                         'otherkey': 'otherval'},
+                                'net3':  {'otherkey': 'otherval'}}}
+        test_mod.populate_network_variables(inventory, inv_src)
+        nets = {'net1': {'network': '10.5.0.0',
+                         'netmask': '255.255.252.0',
+                         'otherkey': 'otherval'},
+                'net2': {'otherkey': 'otherval'},
+                'net3': {'otherkey': 'otherval'}}
+        expected_output['all']['vars']['networks'] = nets
+        #self.maxDiff = None
+        self.assertDictEqual(inventory, expected_output)
+
+    def test_populate_host_networks(self):
+        # Set up test input
+        inventory = {'_meta': {'hostvars': {}}}
+        net_list = ['net1', 'net2', 'net3', 'net4']
+        ihv = inventory['_meta']['hostvars']
+        for x in range(5):
+            ihv['nodeIP%s' % x] = {'host_networks': {}}
+        ip_to_node = {'nodeIP0': {'net1-addr': '1.1.1.1',
+                                  'net2-addr': '2.2.2.1',
+                                  'net4-addr': '4.4.4.1'},
+                      'nodeIP1': {'net3-addr': '3.3.3.2'},
+                      'nodeIP2': {'net1-addr': '1.1.1.3',
+                                  'net2-addr': '',
+                                  'net3-addr': '3.3.3.3'},
+                      'nodeIP3': {'net1-addr': '1.1.1.4',
+                                  'net3-addr': '3.3.3.4',
+                                  'net4-addr': '4.4.4.4'},
+                      'nodeIP4': {'net2-addr': '2.2.2.5',
+                                  'net3-addr': '3.3.3.5'}
+                      }
+
+        expected_output = copy.deepcopy(inventory)
+        hv = expected_output['_meta']['hostvars']
+        hv['nodeIP0']['host_networks'] = {'net1': {'addr': '1.1.1.1'},
+                                          'net2': {'addr': '2.2.2.1'},
+                                          'net4': {'addr': '4.4.4.1'}}
+        hv['nodeIP1']['host_networks'] = {'net3': {'addr': '3.3.3.2'}}
+        hv['nodeIP2']['host_networks'] = {'net1': {'addr': '1.1.1.3'},
+                                          'net2': {},
+                                          'net3': {'addr': '3.3.3.3'}}
+        hv['nodeIP3']['host_networks'] = {'net1': {'addr': '1.1.1.4'},
+                                          'net3': {'addr': '3.3.3.4'},
+                                          'net4': {'addr': '4.4.4.4'}}
+        hv['nodeIP4']['host_networks'] = {'net2': {'addr': '2.2.2.5'},
+                                          'net3': {'addr': '3.3.3.5'}}
+        test_mod.populate_host_networks(inventory, net_list, ip_to_node)
+        #import json
+        #print 'Output %s' % json.dumps(inventory, indent=4)
+        #print 'Expected_output %s' % json.dumps(expected_output, indent=4)
+        self.assertDictEqual(inventory, expected_output)
+
+    @mock.patch(TEST_PKG_MOD+'.populate_rename_interfaces')
+    @mock.patch(TEST_PKG_MOD+'.populate_host_networks')
+    @mock.patch(TEST_PKG_MOD+'.populate_network_variables')
+    @mock.patch(TEST_PKG_MOD+'.populate_hosts')
+    @mock.patch(TEST_PKG_MOD+'.get_host_ip_to_node')
+    @mock.patch(TEST_PKG_MOD+'.load_input_file')
+    def test_generate_dynamic_inventory(self, load, get_host_ip_to_node,
+                                        populate_hosts,
+                                        populate_network_variables,
+                                        populate_host_networks,
+                                        populate_rename_interfaces):
+        ret = test_mod.generate_dynamic_inventory()
+        load.assert_any_call()
+        get_host_ip_to_node.assert_called_once_with(load.return_value)
+        populate_hosts.assert_called_once_with(mock.ANY,
+                                               mock.ANY)
+        populate_network_variables.assert_called_once_with(mock.ANY,
+                                                           load.return_value)
+        populate_rename_interfaces.assert_called_once_with(
+            mock.ANY, load.return_value, get_host_ip_to_node.return_value)
+        self.assertTrue(populate_host_networks.called)
+        expected_output = {'all': {'hosts': [],
+                                   'vars': {}
+                                   },
+                           '_meta': {'hostvars': {}}}
+        self.assertDictEqual(ret, expected_output)
+
+    def test_populate_rename_interfaces(self):
+        inventory = {'all': {'hosts': ['nodeIP0', 'nodeIP1', 'nodeIP2'],
+                             'vars': {}
+                             },
+                     '_meta': {'hostvars': {'nodeIP0': {},
+                                            'nodeIP1': {},
+                                            'nodeIP2': {}}}
+                     }
+        source = {'node_templates': {
+            'compute': {
+                'rename-interfaces': [
+                    {'p1p1': 'eth10'},
+                    {'p1p2': 'eth20'}]},
+            'controller': {
+                'rename-interfaces': [
+                    {'p3p1': 'eth10'},
+                    {'p4p2': 'eth20'}]},
+            'osd': {
+                'rename-interfaces': [
+                    {'p1p7': 'eth10'},
+                    {'p1p8': 'eth20'}]}}}
+
+        ip_to_node = {'nodeIP0': {'template': 'compute'},
+                      'nodeIP1': {'template': 'controller'},
+                      'nodeIP2': {'template': 'osd'}}
+
+        test_mod.populate_rename_interfaces(inventory,
+                                            source, ip_to_node)
+        self.assertEqual(inventory['all']['vars']['node_templates'],
+                         source['node_templates'])
+        hv = inventory['_meta']['hostvars']
+        self.assertEqual(hv['nodeIP0']['template'], 'compute')
+        self.assertEqual(hv['nodeIP1']['template'], 'controller')
+        self.assertEqual(hv['nodeIP2']['template'], 'osd')
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
