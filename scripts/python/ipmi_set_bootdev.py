70782c5fc00d1f7908b1773a391eb2b62ffd8096
Jay Carman <jwcarman@us.ibm.com>
2016-06-27 13:30:18 -0500

    Updates to ipmi_set_bootdev

--- scripts/python/ipmi_set_bootdev.py
+++ scripts/python/ipmi_set_bootdev.py
@@ -17,6 +17,9 @@ class IpmiSetBootdev(object):
         if log_level is not None:
             log.set_level(log_level)
 
+        if type(persist) is not bool:
+            persist = (persist == 'True')
+
         inv = Inventory(log_level, inv_file)
         for rack_id, ipv4, _userid, _password in inv.yield_ipmi_access_info():
             ipmi_cmd = ipmi_command.Command(
@@ -32,6 +35,7 @@ class IpmiSetBootdev(object):
                     'Rack: %s - IP: %s, %s' %
                     (bootdev, persist, rack_id, ipv4, str(error)))
                 sys.exit(1)
+
             if 'error' in rc:
                 log.error(
                     'set_bootdev failed (device=%s persist=%s) - '
@@ -39,11 +43,34 @@ class IpmiSetBootdev(object):
                     (bootdev, persist, rack_id, ipv4, str(rc['error'])))
                 sys.exit(1)
 
-            log.info(
-                    'bootdev set to (device=%s persist=%s) - '
+            time.sleep(5)
+
+            try:
+                rc = ipmi_cmd.get_bootdev()
+            except pyghmi_exception.IpmiException as error:
+                log.error(
+                    'get_bootdev failed - '
+                    'Rack: %s - IP: %s, %s' %
+                    (rack_id, ipv4, str(error)))
+                sys.exit(1)
+
+            if 'error' in rc:
+                log.error(
+                    'get_bootdev failed - '
+                    'Rack: %s - IP: %s, %s' %
+                    (rack_id, ipv4, str(rc['error'])))
+                sys.exit(1)
+            elif rc['bootdev'] == bootdev and str(rc['persistent']) == str(persist):
+                log.info(
+                    'set_bootdev successful (device=%s persist=%s) - '
                     'Rack: %s - IP: %s' %
                     (bootdev, persist, rack_id, ipv4))
-        time.sleep(30)
+            else:
+                log.error(
+                    'set_bootdev failed - set: (device=%s persist=%s) but read: (device=%s persist=%s) - '
+                    'Rack: %s - IP: %s' %
+                    (bootdev, persist, rc['bootdev'], rc['persistent'], rack_id, ipv4))
+                sys.exit(1)
 
 
 if __name__ == '__main__':
