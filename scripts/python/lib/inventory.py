2ec8b2b9d2c9aac0759b6bd9dd2d3ee48a1421b7
Jay Carman <jwcarman@us.ibm.com>
2016-06-29 16:03:30 -0500

    Missing parenthesis

--- scripts/python/lib/inventory.py
+++ scripts/python/lib/inventory.py
@@ -170,7 +170,7 @@ class Inventory():
                                     _dict[CFG_USERID_DATA_SWITCH] = userid
                                     _dict[CFG_PASSWORD_DATA_SWITCH] = password
                                     if (CFG_VLAN in self.cfg[CFG_NETWORKS][networks] and 
-                                        self.cfg[CFG_NETWORKS][networks][CFG_VLAN] not in _list:
+                                        self.cfg[CFG_NETWORKS][networks][CFG_VLAN] not in _list):
                                         _list.append(self.cfg[CFG_NETWORKS][networks][CFG_VLAN])
                                         _dict['vlan'] = _list
                                         __dict[self.cfg[CFG_IPADDR_DATA_SWITCH][rack]] = _dict
