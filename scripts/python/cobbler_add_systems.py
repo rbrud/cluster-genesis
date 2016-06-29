de628f7ce30438124a6db80875d69c12be1840d3
Jay Carman <jwcarman@us.ibm.com>
2016-06-29 11:47:16 -0500

    Fixes to cobbler_add_systems.py refactoring.
    Also added system model and part number information in cobbler
    comment sections (if information is available via ipmi).

--- scripts/python/cobbler_add_systems.py
+++ scripts/python/cobbler_add_systems.py
@@ -3,10 +3,10 @@ from __future__ import nested_scopes, generators, division, absolute_import, \
     with_statement, print_function, unicode_literals
 import sys
 import os.path
-import yaml
 from orderedattrdict.yamlutils import AttrDictYAMLLoader
 import xmlrpclib
 
+from lib.inventory import Inventory
 from lib.logger import Logger
 
 COBBLER_USER = 'cobbler'
@@ -18,6 +18,10 @@ YAML_USERID_IPMI = 'userid-ipmi'
 YAML_PASSWORD_IPMI = 'password-ipmi'
 YAML_IPV4_PXE = 'ipv4-pxe'
 YAML_MAC_PXE = 'mac-pxe'
+YAML_CHASSIS_PART_NUMBER = 'chassis-part-number'
+YAML_CHASSIS_SERIAL_NUMBER = 'chassis-serial-number'
+YAML_MODEL = 'model'
+YAML_SERIAL_NUMBER = 'serial-number'
 
 YAML_COBBLER_PROFILE = 'cobbler-profile'
 YAML_ARCH = 'architecture'
@@ -37,82 +41,105 @@ class CobblerAddSystems(object):
         inv = Inventory(log_level, inv_file, cfg_file)
 
         for node_inv, INV_NODES, key, index, node in inv.yield_nodes():
-           hostname = node_inv[key][index][YAML_HOSTNAME]
-           ipv4_ipmi = node_inv[key][index][YAML_IPV4_IPMI]
-           userid_ipmi = node_inv[key][index][YAML_USERID_IPMI]
-           password_ipmi = node_inv[key][index][YAML_PASSWORD_IPMI]
-           ipv4_pxe = node_inv[key][index][YAML_IPV4_PXE]
-           mac_pxe = node_inv[key][index][YAML_MAC_PXE]
+            hostname = node[YAML_HOSTNAME]
+            ipv4_ipmi = node[YAML_IPV4_IPMI]
+            userid_ipmi = node[YAML_USERID_IPMI]
+            password_ipmi = node[YAML_PASSWORD_IPMI]
+            ipv4_pxe = node[YAML_IPV4_PXE]
+            mac_pxe = node[YAML_MAC_PXE]
 
-           if YAML_COBBLER_PROFILE in node_inv[key][index]:
-               COBBLER_PROFILE = \
-                   node_inv[key][index][YAML_COBBLER_PROFILE]
-           elif (YAML_ARCH in node_inv[key][index] and
-                   node_inv[key][index][YAML_ARCH] is not None):
-               if node_inv[key][index][YAML_ARCH].lower() == 'x86_64':
-                   COBBLER_PROFILE = COBBLER_PROFILE_X86_64
-               elif node_inv[key][index][YAML_ARCH].lower() == 'ppc64':
-                   COBBLER_PROFILE = COBBLER_PROFILE_PPC64
-               else:
-                   self.log.log_error(
-                       'Inventory: Invalid architecture set for ' +
-                       hostname)
-           else:
-               COBBLER_PROFILE = COBBLER_PROFILE_X86_64
+            if YAML_COBBLER_PROFILE in node:
+                COBBLER_PROFILE = \
+                    node[YAML_COBBLER_PROFILE]
+            elif (YAML_ARCH in node and
+                    node[YAML_ARCH] is not None):
+                if node[YAML_ARCH].lower() == 'x86_64':
+                    COBBLER_PROFILE = COBBLER_PROFILE_X86_64
+                elif node[YAML_ARCH].lower() == 'ppc64':
+                    COBBLER_PROFILE = COBBLER_PROFILE_PPC64
+                else:
+                    log.log_error(
+                        'Inventory: Invalid architecture set for ' +
+                        hostname)
+            else:
+                COBBLER_PROFILE = COBBLER_PROFILE_X86_64
 
-           new_system_create = cobbler_server.new_system(token)
+            new_system_create = cobbler_server.new_system(token)
 
-           cobbler_server.modify_system(
-               new_system_create,
-               "name",
-               hostname,
-               token)
-           cobbler_server.modify_system(
-               new_system_create,
-               "hostname",
-               hostname,
-               token)
-           cobbler_server.modify_system(
-               new_system_create,
-               "power_address",
-               ipv4_ipmi,
-               token)
-           cobbler_server.modify_system(
-               new_system_create,
-               "power_user",
-               userid_ipmi,
-               token)
-           cobbler_server.modify_system(
-               new_system_create,
-               "power_pass",
-               password_ipmi,
-               token)
-           cobbler_server.modify_system(
-               new_system_create,
-               "power_type",
-               "ipmilan",
-               token)
-           cobbler_server.modify_system(
-               new_system_create,
-               "profile",
-               COBBLER_PROFILE,
-               token)
-           cobbler_server.modify_system(
-               new_system_create,
-               'modify_interface',
-               {
-                   "macaddress-eth0": mac_pxe,
-                   "ipaddress-eth0": ipv4_pxe,
-                   "dnsname-eth0": hostname},
-               token)
+            cobbler_server.modify_system(
+                new_system_create,
+                "name",
+                hostname,
+                token)
+            cobbler_server.modify_system(
+                new_system_create,
+                "hostname",
+                hostname,
+                token)
+            cobbler_server.modify_system(
+                new_system_create,
+                "power_address",
+                ipv4_ipmi,
+                token)
+            cobbler_server.modify_system(
+                new_system_create,
+                "power_user",
+                userid_ipmi,
+                token)
+            cobbler_server.modify_system(
+                new_system_create,
+                "power_pass",
+                password_ipmi,
+                token)
+            cobbler_server.modify_system(
+                new_system_create,
+                "power_type",
+                "ipmilan",
+                token)
+            cobbler_server.modify_system(
+                new_system_create,
+                "profile",
+                COBBLER_PROFILE,
+                token)
+            cobbler_server.modify_system(
+                new_system_create,
+                'modify_interface',
+                {
+                    "macaddress-eth0": mac_pxe,
+                    "ipaddress-eth0": ipv4_pxe,
+                    "dnsname-eth0": hostname},
+                token)
+            comment = ""
+            if YAML_CHASSIS_PART_NUMBER in node:
+                comment += (
+                    '%s: %s\n' %
+                    (YAML_CHASSIS_PART_NUMBER, node[YAML_CHASSIS_PART_NUMBER]))
+            if YAML_CHASSIS_SERIAL_NUMBER in node:
+                comment += (
+                    '%s: %s\n' %
+                    (YAML_CHASSIS_SERIAL_NUMBER,
+                        node[YAML_CHASSIS_SERIAL_NUMBER]))
+            if YAML_MODEL in node:
+                comment += (
+                    '%s: %s\n' %
+                    (YAML_MODEL, node[YAML_MODEL]))
+            if YAML_SERIAL_NUMBER in node:
+                comment += (
+                    '%s: %s\n' %
+                    (YAML_SERIAL_NUMBER, node[YAML_SERIAL_NUMBER]))
+            cobbler_server.modify_system(
+                new_system_create,
+                "comment",
+                comment,
+                token)
 
-           cobbler_server.save_system(new_system_create, token)
+            cobbler_server.save_system(new_system_create, token)
 
-           self.log.info(
-               "Cobbler Add System: " +
-               "name=" + hostname + ", " +
-               "profile=" + COBBLER_PROFILE
-               )
+            log.info(
+                "Cobbler Add System: " +
+                "name=" + hostname + ", " +
+                "profile=" + COBBLER_PROFILE
+                )
 
         cobbler_server.sync(token)
 
@@ -124,7 +151,7 @@ if __name__ == '__main__':
     """
     log = Logger(__file__)
 
-    ARGV_MAX = 3
+    ARGV_MAX = 4
     argv_count = len(sys.argv)
     if argv_count > ARGV_MAX:
         try:
