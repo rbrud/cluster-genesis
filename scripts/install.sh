485c6cd9b5200749aca792d5d039bbd3f2097e31
Jay Carman <jwcarman@us.ibm.com>
2016-08-10 16:08:23 -0500

    add shebang to install shell script
    Right now we only support Ubuntu OS On deployer system so it's probably
    pretty safe to assume user is running script from bash BUT adding the
    shebang to explicitly call bash is better.

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 DISTRIB_RELEASE=$(lsb_release -sr)
 sudo apt-get -y install python-pip python-dev libffi-dev libssl-dev python-netaddr ipmitool
 if [[ $DISTRIB_RELEASE == "14.04" ]]; then
@@ -7,7 +8,7 @@ sudo -H pip install --upgrade pip
 sudo -H pip install --upgrade setuptools
 sudo -H pip install --upgrade wheel
 if [[ $DISTRIB_RELEASE == "14.04" ]]; then
-    sudo -H pip install lxc-python2 
+    sudo -H pip install lxc-python2
 fi
 sudo -H pip install virtualenv
 virtualenv --no-wheel --system-site-packages deployenv
